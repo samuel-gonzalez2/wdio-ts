e8a70bd779d4c10e26e803b8a473d6e4d9f18d83
samuel-gonzalez2 <samuel.gonzalez@enroutesystems.com>
2022-06-20 21:43:12 -0500

    [CREATED] POM pattern and TS module completed

--- test/pageobjects/AbstractPage.ts
+++ test/pageobjects/AbstractPage.ts
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 export default class AbstractPage {
     public async waitForSeconds(seconds: number) {
         await browser.pause(seconds * 1000);
@@ -11,4 +13,15 @@ export default class AbstractPage {
         await browser.throttle(speed)
     }
 
+    public async takeScreenshot(name: string, path: string) {
+        await browser.saveScreenshot(`${name}.png, ${path}`);
+    }
+
+    public async generateRandomNumber(){
+        return Math.floor(Math.random() * 100000 + 1);
+    }
+
+    public async generateRandomString(length: number) {
+        return crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length);
+    }
 }
